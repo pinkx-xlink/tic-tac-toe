910242a48db4cb1c997c15db172fdf9d42aedec3
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-18 22:43:56 -0400

    Add eventListeners to btn...does not work yet

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function Gameboard() {
     let array = [1, 2, 3];
+    let button = document.querySelector("#gameBtn");
+    button.addEventListener("click", updateArray);
     const board = [];
+}
+
+function updateArray() {
+    array.push(4);
+    console.log(array);
 }
 
 const getBoard = () => board;
