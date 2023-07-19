118d19e026373941e6d73bff64a8c7d785d22c0f
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-19 13:05:10 -0400

    Comment out array code lines 10-17

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ function Gameboard() {
     const board = [];
 }
 
-let array = [1, 2, 3];
-let button = document.querySelector("#gameBtn");
-button.addEventListener("click", updateArray);
 
-function updateArray() {
-    array.push(4);
-    console.log(array);
-}
 
-const getBoard = () => board;
+
+
+// let array = [1, 2, 3];
+// let button = document.querySelector("#gameBtn");
+// button.addEventListener("click", updateArray);
+
+// function updateArray() {
+//     array.push(4);
+//     console.log(array);
+// }
+
 
 // const takeTurn = (array, player) => {
 
