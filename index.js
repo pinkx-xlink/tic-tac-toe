391532dbba5d88989d575d052d49169e00e92550
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-19 13:28:22 -0400

    ADD WINNING COMB. FUNC

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ function Gameboard() {
 
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0 3 6]
+    [1 4 7]
+    [2 5 8]
+    [0 4 8]
+    [2 4 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById()
 let circleTurn
 
 function startGame() {
+    circleTurn = false
     cellElements.forEach(cell => {
         cell.addEventListener('click', handleClick, { once: true })
     })
-    
+    setBoardHoverClass()
 }
 
 
