a5091bbd46f62de3ed6ce32395558615b3c060da
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-19 13:09:02 -0400

    Add circle and x classes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function Gameboard() {
     const board = [];
 }
 
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
 const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn
 
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, { once: true })
